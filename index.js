b4da743f0537958fe87ee2f3067a153c162444f3
kyu999 <kyukokkyou999@gmail.com>
2018-10-05 18:26:01 +0900

    enable drag and drop

--- index.js
+++ index.js
@@ -33,3 +33,40 @@ init_protparams = [new ProtParam('')]
 var aa_composition_chart = visualize_aa_composition(init_protparams)
 var kd_hydrophobicity_chart = visualize_kd_hydrophobicity(init_protparams)
 var molecular_weight_chart = visualize_molecular_weight(init_protparams)
+
+
+$(function() {
+        var droppable = $("#droppable");
+
+        if(!window.FileReader) {
+          alert("File API がサポートされていません。");
+          return false;
+        }
+
+        var cancelEvent = function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        }
+
+        droppable.bind("dragenter", cancelEvent);
+        droppable.bind("dragover", cancelEvent);
+
+        var handleDroppedFile = function(event) {
+          var files = event.originalEvent.dataTransfer.files
+          var file = files[files.length-1]; // always get up-to-date file, and allow only single file
+
+          var fileReader = new FileReader();
+          fileReader.onload = function(event) {
+            protein_analysis.fasta = event.target.result
+          }
+          fileReader.readAsText(file);
+
+          // デフォルトの処理をキャンセルします.
+          cancelEvent(event);
+          return false;
+        }
+
+        // ドロップ時のイベントハンドラを設定します.
+        droppable.bind("drop", handleDroppedFile);
+      });
